8001d65396c4020907291024780e6781ba669935
zhcanran <1215355076@qq.com>
2023-08-04 09:27:51 +0800

    我改了一下，其实没改

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,7 +1,7 @@
 import collections
 
+# Card = collections.namedtuple('Card', ['rank', 'suit'])
 Card = collections.namedtuple('Card', ['rank', 'suit'])
-
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
